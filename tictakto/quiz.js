ba2dded8bc7796aa319bcff0fa020a270d243bd0
cnsahil10 <ssahileo770@gmail.com>
2024-06-25 18:51:17 +0530

    Update quiz.js

--- tictakto/quiz.js
+++ tictakto/quiz.js
@@ -1,12 +1,1 @@
-console.log("hi");
-function getdata(){
-    fetch("https://dreamthemonline.com/sample/getNewQuizData/600")
-    .then((response)=>{
-        
-        return response.json()
-    }).then(data=>{
-        console.log(data)
-    })
-     
-}
-getdata();
+
